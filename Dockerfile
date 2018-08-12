5f4a48e7bda43b8397deb483c97f6f7e1b60932b
Tiago Resende <tiago@nibo.com.br>
2018-08-12 19:52:21 -0300

    Implementa configuração docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM microsoft/dotnet:2.1-sdk AS builder
+
+WORKDIR /app
+
+COPY . .
+RUN dotnet publish . -c Release -o dist
+
+FROM microsoft/dotnet:2.1-aspnetcore-runtime
+WORKDIR /app
+
+COPY --from=builder /app/src/Api/dist ./
+
+ENTRYPOINT dotnet Products.Api.dll
--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -28,6 +28,15 @@ namespace Products.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
             services.RegisterDependencies(this.Configuration);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
@@ -47,12 +56,16 @@ namespace Products.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors("CorsPolicy");
+
             app.UseMiddleware<ExceptionHandlerMiddleware>();
             app.UseMvc();
 
